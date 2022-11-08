c0dee3b902483a4ce4431af2306e971da2490c14
Vinod Soni <vinodsoni@microsoft.com>
2022-11-08 16:20:19 -0500

    Both issue fixed and closed #1 & #2

--- MicrosoftWebApp/SomeLogic.cs
+++ MicrosoftWebApp/SomeLogic.cs
@@ -8,6 +8,7 @@
         /// <returns></returns>
         int ArraySorting()
         {
+            // Let me correct the business logic here - fixed
             int[] numbers = new int[] { 3, 5, 7, 1, 2 };
             Array.Sort(numbers);
             return numbers[0];
@@ -20,6 +21,7 @@
         /// <returns></returns>
         string FindLastWord(string sentence)
         {
+            // Let me correct the business logic here - fixed
             string[] words = sentence.Split(' ');
             return words[words.Length - 1];
         }
